9bf8bc621e93af2e39a626750b2142101fc0d595
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2023-04-12 16:01:43 +0300

    Remove defaults

--- src/components/Todos/List.jsx
+++ src/components/Todos/List.jsx
@@ -1,7 +1,7 @@
 import ListItem from "./ListItem";
 import "./Todos.css";
 
-export default function List({ todos, onCheck = () => {}, onRemove = () => {} }) {
+export default function List({ todos, onCheck, onRemove }) {
   return (
     <ul className="TodosList">
       {todos.map((todo) => <ListItem key={todo.id} todo={todo} onCheck={onCheck} onDelete={onRemove} />)}
